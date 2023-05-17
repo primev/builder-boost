1e3dd380679cdfbf281a26e293833745b0c04e4b
Serhii Varakuta <flood.code@gmail.com>
2023-05-17 04:13:36 +0300

    Update GetCommitment() utils function

--- pkg/utils/commitment.go
+++ pkg/utils/commitment.go
@@ -7,5 +7,5 @@ import (
 
 // GetCommitment calculates commitment by pseudo searcher and builder addresses
 func GetCommitment(commitmentAddress, builder common.Address) common.Hash {
-	return crypto.Keccak256Hash(commitmentAddress[:], builder[:])
+	return crypto.Keccak256Hash(commitmentAddress.Bytes(), builder.Bytes())
 }
