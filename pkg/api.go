364cbb4fb874dcbce64cedc35c6340e3420b96db
ckartik <k5chopra@uwaterloo.ca>
2023-05-17 10:56:02 -0700

    Introduces a heartbeat for the worker when it's not blocked.

--- pkg/api.go
+++ pkg/api.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 
@@ -224,7 +225,8 @@ func succeed(status int) http.HandlerFunc {
 }
 
 type healthCheck struct {
-	Searchers []string `json:"connected_searchers"`
+	Searchers       []string  `json:"connected_searchers"`
+	WorkerHeartBeat time.Time `json:"worker_heartbeat"`
 }
 
 // healthCheck detremines if the service is healthy
@@ -238,6 +240,7 @@ func (a *API) handleHealthCheck(w http.ResponseWriter, r *http.Request) {
 		searchers = append(searchers, searcher)
 	}
 	a.Worker.lock.RUnlock()
+
 	// Send the list over the API
-	json.NewEncoder(w).Encode(healthCheck{Searchers: searchers})
+	json.NewEncoder(w).Encode(healthCheck{Searchers: searchers, WorkerHeartBeat: time.Unix(a.Worker.GetHeartbeat(), 0)})
 }
