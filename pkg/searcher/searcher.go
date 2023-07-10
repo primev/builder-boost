ff9f50fa4f289144ddf231339cbdcfbb5468bb1c
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-07-10 12:07:09 -0400

    Reverts error handling changes.

--- pkg/searcher/searcher.go
+++ pkg/searcher/searcher.go
@@ -166,9 +166,7 @@ func (s *searcher) processMessages(c *websocket.Conn) error {
 	for {
 		_, message, err := c.ReadMessage()
 		now = time.Now()
-		if err != nil && err.(*websocket.CloseError).Code == websocket.CloseAbnormalClosure {
-			s.log.Error(err.Error())
-		} else if err != nil {
+		if err != nil {
 			return err
 		}
 		var m boost.Metadata
